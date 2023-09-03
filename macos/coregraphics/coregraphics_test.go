755322aeb0523b652dc5d460a6ca2f0c0b9e5a95
Travis Cline <travis.cline@gmail.com>
2023-09-02 20:02:05 -0700

    coregraphics: Add basic test

--- macos/coregraphics/coregraphics_test.go
+++ macos/coregraphics/coregraphics_test.go
@@ -3,3 +3,10 @@ package coregraphics
 import "testing"
 
 func TestCoreGraphicsValid(t *testing.T) {}
+
+func TestDisplayCreateImage(t *testing.T) {
+	id := MainDisplayID()
+	t.Logf("MainDisplayID: %d", id)
+	img := DisplayCreateImage(id)
+	t.Logf("DisplayCreateImage: %v", img)
+}
