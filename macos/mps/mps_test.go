6d4f95f725a9760b5a7c0a3699c9cb67fff49419
Travis Cline <travis.cline@gmail.com>
2023-09-06 20:39:24 -0700

    mps: Add additional structs

--- macos/mps/mps_test.go
+++ macos/mps/mps_test.go
@@ -1,5 +1,31 @@
 package mps
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/progrium/macdriver/macos/metal"
+)
 
 func TestMPSValid(t *testing.T) {}
+
+func TestX(t *testing.T) {
+	d := metal.CreateSystemDefaultDevice()
+	cq := d.NewCommandQueue()
+	cb := cq.CommandBuffer()
+
+	mm := NewMatrixMultiplication()
+	m1 := NewMatrix()
+	m2 := NewMatrix()
+	m3 := NewMatrix()
+
+	mm.EncodeToCommandBufferLeftMatrixRightMatrixResultMatrix(cb, m1, m2, m3)
+
+	//ce.SetComputePipelineState(
+	fmt.Println(cb.Status())
+	cb.Commit()
+	fmt.Println(cb.Status())
+	fmt.Println(cb.Status())
+	fmt.Println(cb.Status())
+	fmt.Println(cb.Status())
+}
